76dd24b8bec7fc44a3529513fea90dfbd919aabd
idelc <idel012@ucr.edu>
2021-12-08 22:52:20 -0800

    changes for report

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <ctime>
 
 #include "fileRead.h"
 #include "methods.h"
@@ -10,9 +11,15 @@ int main(){
     double small[500][11];
     double big[2000][51]; 
 
-    readData(small);
-    backwardElimination(small);
-    forwardSelection(small);
+    readData(big);
+    
+    clock_t runTime = clock();
+    backwardElimination(big);
+    cout << "\nCPU time (linux only): " << static_cast<double>((clock()-runTime))/CLOCKS_PER_SEC << " seconds" << endl;
+
+    runTime = clock();
+    forwardSelection(big);
+    cout << "\nCPU time (linux only): " << static_cast<double>((clock()-runTime))/CLOCKS_PER_SEC << " seconds" << endl;
 
     return 0;
 }
